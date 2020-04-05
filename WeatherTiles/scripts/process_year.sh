b580b063ae1a2eadb7b1fec45122a668d7d6aafd
tmutmu1 <tmutmu111@gmail.com>
2020-04-04 21:33:53 -0700

    Improved parallelization for processing images

--- WeatherTiles/scripts/process_year.sh
+++ WeatherTiles/scripts/process_year.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-
 mkdir -p data
 mkdir -p output
+mkdir -p output/conus/
+numjobs=2
 
-#FILECOUNT=$(ls -f mtarchive.geol.iastate.edu/*/*/*/mrms/ncep/SeamlessHSR/*.grib2.gz | wc -l)
-FILECOUNT=1
-INDEX=0
-maxjobs=8
-
-
-echo "Files:" $FILECOUNT
-for file in mtarchive.geol.iastate.edu/*/*/*/mrms/ncep/SeamlessHSR/*.grib2.gz; do
-    FILE=$file
+runprocess() {
+    FILE=$1
     DATE=`echo $FILE | cut -f 3 -d _ | cut -f 1 -d .`
-    mkdir -p output/conus/
     if [ ! -f  output/conus/CONUS_$DATE.png  ]; then
        THECMD="./create_mrms_rain_conus.sh  $FILE output/conus/CONUS_$DATE.png" 
-       #echo $THECMD
-       if (( $(($((++n)) % $maxjobs)) == 0 )) ; then
-       	$THECMD &
-       fi
+       $THECMD
 
-       #let "INDEX = $((INDEX+1))"
-       #PERCENTCOMPLETE=$((100*INDEX/FILECOUNT))
-       #echo  "$PERCENTCOMPLETE% Complete"
      fi 
-done
+}
+export -f runprocess
+
+find -type f -wholename './mtarchive.geol.iastate.edu/*/*/*/mrms/ncep/SeamlessHSR/*.grib2.gz' | sort -g | parallel -j $numjobs runprocess
