2f1a7d0e9c758685859fba56a65a2f2add7783bd
tmutmu1 <tmutmu111@gmail.com>
2020-05-18 21:39:09 -0700

    Tweaks to image processing

--- WeatherTiles/scripts/create_combined.sh
+++ WeatherTiles/scripts/create_combined.sh
@@ -60,7 +60,7 @@ if [ $dataType = "mrms" ] || [ $dataType = "hrrr" ]; then
 fi
 
 
-if [ $dataType = "hrrr" ]; then
+if [ $dataType = "hrrr" ] || [ $dataType = "hrrr_prs" ]; then
 	grep -qxF "$file" hrrr_processed_images_combined.txt || echo "$file" >> hrrr_processed_images_combined.txt
 fi
 
