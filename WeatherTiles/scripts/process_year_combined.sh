7dd0e9995ec40b317bed24d2ea9ee5b19a278507
tmutmu1 <tmutmu111@gmail.com>
2020-05-18 21:42:14 -0700

    Tweaks to image processing

--- WeatherTiles/scripts/process_year_combined.sh
+++ WeatherTiles/scripts/process_year_combined.sh
@@ -15,7 +15,7 @@ runprocessmrms() {
     HOUR="`echo $FILE | cut -f 2 -d - | cut -f 1 -d .`"
     if [[ $HOUR == [0-2][0-9]0000 ]]; then
         if [ ! -f  output/combined/mrms/CONUS_$DATE.png  ]; then
-           THECMD="./create_rain_combined.sh  $FILE output/combined/mrms/CONUS_$DATE.png mrms" 
+           THECMD="./create_combined.sh  $FILE output/combined/mrms/CONUS_$DATE.png mrms" 
            $THECMD
         fi 
     fi 
@@ -30,7 +30,7 @@ runprocesshrrr() {
     HOUR=`echo ${FILE} | cut -f 2 -d . | cut -f 3 -d . | cut -f 2 -d t | cut -f 1 -d z`
     DATE=${YEAR}${MONTH}${DAY}-${HOUR}0000
     if [ ! -f  output/combined/hrrr/CONUS_$DATE.png  ]; then
-       THECMD="./create_rain_combined.sh  $FILE output/combined/hrrr/CONUS_$DATE.png hrrr" 
+       THECMD="./create_combined.sh  $FILE output/combined/hrrr/CONUS_$DATE.png hrrr" 
        $THECMD
 
      fi 
@@ -45,20 +45,20 @@ runprocesshrrrprs() {
     HOUR=`echo ${FILE} | cut -f 2 -d . | cut -f 3 -d . | cut -f 2 -d t | cut -f 1 -d z`
     DATE=${YEAR}${MONTH}${DAY}-${HOUR}0000
     if [ ! -f  output/combined/hrrr_pressure_velocity/CONUS_$DATE.png  ]; then
-       THECMD="./create_rain_combined.sh  $FILE output/combined/hrrr_pressure_velocity/CONUS_$DATE.png hrrr_pressure_velocity" 
+       THECMD="./create_combined.sh  $FILE output/combined/hrrr_pressure_velocity/CONUS_$DATE.png hrrr_pressure_velocity" 
        $THECMD
     fi
     if [ ! -f  output/combined/hrrr_pressure/CONUS_$DATE.png  ]; then
-       THECMD="./create_rain_combined.sh  $FILE output/combined/hrrr_pressure/CONUS_$DATE.png hrrr_pressure" 
+       THECMD="./create_combined.sh  $FILE output/combined/hrrr_pressure/CONUS_$DATE.png hrrr_pressure" 
        $THECMD
     fi
     if [ ! -f  output/combined/hrrr_temperature/CONUS_$DATE.png  ]; then
-       THECMD="./create_rain_combined.sh  $FILE output/combined/hrrr_temperature/CONUS_$DATE.png hrrr_temperature" 
+       THECMD="./create_ombined.sh  $FILE output/combined/hrrr_temperature/CONUS_$DATE.png hrrr_temperature" 
        $THECMD
     fi 
 }
 export -f runprocesshrrrprs
 
-find mtarchive.geol.iastate.edu/*/*/*/mrms/ncep/SeamlessHSR/ -name "*.grib2.gz" | sort -g | parallel -j $numjobs runprocessmrms
-find hrrr/f01/*/*/*/ -name "*.grib2" | sort -g | parallel -j $numjobs runprocesshrrr
+# find mtarchive.geol.iastate.edu/*/*/*/mrms/ncep/SeamlessHSR/ -name "*.grib2.gz" | sort -g | parallel -j $numjobs runprocessmrms
+# find hrrr/f01/*/*/*/ -name "*.grib2" | sort -g | parallel -j $numjobs runprocesshrrr
 find hrrr_prs/f00/*/*/*/ -name "*.grib2" | sort -g | parallel -j $numjobs runprocesshrrrprs
